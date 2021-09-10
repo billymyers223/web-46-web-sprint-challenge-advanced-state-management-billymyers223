a190af7f8de5157bd3d37f95be514763b81fa1af
billmyers223 <billymyers223@gmail.com>
2021-09-10 12:50:36 -0500

    Set up actions and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,54 @@
+import {FETCH_START, FETCH_SUCCESS, FETCH_FAIL, ADD_SMURF, SET_ERROR} from "../actions/index.js"
+
 
 export const initialState = {
-    smurfs: [],
+    smurfs: [
+        {
+            id:'',
+            name:'',
+            position:'',
+            description:''
+        }
+    ],
     isLoading: false,
     error:''
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action)=>{
+    switch (action.type){
+        case(FETCH_START):
+            return({
+                ...state,
+                isLoading:true,
+                error:''
+            })
+        case(FETCH_SUCCESS):
+            return({
+                ...state,
+                smurfs:action.payload,
+                isLoading:false
+            })
+        case(FETCH_FAIL):
+            return({
+                ...state,
+                error:action.payload
+            })
+        case(ADD_SMURF):
+            return({
+                ...state,
+                smurfs:[
+                    ...state.smurfs, action.payload
+                ]
+            })
+        case(SET_ERROR):
+            return({
+                ...state,
+                error: action.payload
+            })
+
+        default:
+            return state;
+    }
 }
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
